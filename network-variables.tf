5fc1a284295ed854b6c7ba7e874230899b0a5594
mukeshmcse <mukeshmcitp@gmail.com>
2022-12-01 09:56:23 +0530

    terraform file ready

---
+++ network-variables.tf
@@ -0,0 +1,25 @@
+# network varibles | network-single-region.tf
+
+# define GCP region
+variable "us-east4" {
+  type = string
+  description = "GCP region"
+}
+
+# define GCP zone
+variable "gus-east4 -a" {
+  type = string
+  description = "GCP zone"
+  
+  variable "gus-east4 -b" {
+  type = string
+  description = "GCP zone"
+}
+}
+
+# define private subnet
+variable "private_subnet_cidr_1" {
+  type = string
+  description = "private subnet CIDR 1"
+}
+
